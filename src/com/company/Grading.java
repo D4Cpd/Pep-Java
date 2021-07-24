673f632e4546b3a9fcdd1c523df60430ddac61fc
Prince <princedagar71@gmail.com>
2021-07-24 15:41:34 +0530

    Grading

---
+++ src/com/company/Grading.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Grading {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Marks:");
+        int marks=sc.nextInt();
+        if(marks>90 && marks<=100)
+            System.out.println("Excellent");
+        else if (marks>80 && marks<=90)
+            System.out.println("Good");
+        else if (marks>70 && marks<=80)
+            System.out.println("Fair");
+        else if(marks>60 && marks<=70)
+            System.out.println("Meets Expectation");
+        else if (marks>0 && marks<=60)
+            System.out.println("Below Par");
+        else
+            System.out.println("Wrong Input");
+    }
+}
