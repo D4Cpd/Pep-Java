f4e193fece6bd9f68356d5914fefaf47523c9d61
Prince <princedagar71@gmail.com>
2021-07-25 14:33:31 +0530

    isPrime Done

--- src/com/company/IsPrime.java
+++ src/com/company/IsPrime.java
@@ -2,23 +2,28 @@ package com.company;
 
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 public class IsPrime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number:");
         int n = sc.nextInt();
-        int count=0;
-        for (int i = 2; i <= n; i++) {
-           if(n%i==0)
-               count++;
+        for (int j=0;j<n;j++) {
+            int t= sc.nextInt();
+            int count=0;
+            for (int i = 2; i  <=sqrt(t) ; i++) {
+                if (t % i == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0){
+                System.out.println("Prime");}
+            else{
+                System.out.println("Non Prime");}
         }
-        if (count == 2)
-            System.out.println("Non Prime");
-        else
-            System.out.println("Prime");
-
-
-
     }
-
 }
+
+
