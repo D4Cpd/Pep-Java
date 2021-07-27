a3dc7570c0a580d0abf61bd39bd75e8dadb9c83d
Prince <princedagar71@gmail.com>
2021-07-27 16:59:22 +0530

    Prime Factorisation Complete

--- src/com/company/PrimeFactorisation.java
+++ src/com/company/PrimeFactorisation.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class PrimeFactorisation {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-
-
+        int n= sc.nextInt();
+        for (int i=2;i<=Math.sqrt(n);i++){
+               while (n%i==0){
+                n=n/i;
+                System.out.println(i);}
+        }
+        if(n!=1) {
+            System.out.println(n);
+        }
     }
 }
