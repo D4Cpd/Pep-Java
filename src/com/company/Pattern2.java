e97afb925b40d8a82cebf7fa5f739d1dab50e029
Prince <princedagar71@gmail.com>
2021-07-30 21:13:37 +0530

    Pattern1 & Pattern2 Complete

---
+++ src/com/company/Pattern2.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+
+        public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+            int n=scn.nextInt();
+
+            // write ur code here
+            for(int i=1;i<=n;i++){
+                for(int j=n;j>=1;j--){
+                    System.out.print("*\t");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
