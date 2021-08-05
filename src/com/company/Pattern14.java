af37cbcd9f2e0799e68092b32c06253811ef83e5
Prince <princedagar71@gmail.com>
2021-08-05 21:11:30 +0530

    Pattern14

---
+++ src/com/company/Pattern14.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        // write ur code here
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+
+    }
+}
